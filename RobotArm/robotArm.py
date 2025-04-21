17fca5217bddd931249b08e80bae8fd10bdf6415
JJ-223 <151877132+JJ-223@users.noreply.github.com>
2025-04-21 13:40:18 -0700

    prints out hand land marks

--- RobotArm/robotArm.py
+++ RobotArm/robotArm.py
@@ -31,6 +31,9 @@ with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5) a
 
         # Rendering results
         if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                print (hand_landmarks)
+
             for num, hand in enumerate(results.multi_hand_landmarks):
                 mp_drawing.draw_landmarks(image, hand, mp_hands.HAND_CONNECTIONS,
                                           mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=4),
