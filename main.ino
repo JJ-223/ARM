5ff48a623ffad333d4c8f2a52735ea4372727fd2
URVlDervl <112283216+URVlDervl@users.noreply.github.com>
2025-05-04 16:13:44 -0700

    Update main.ino

--- main.ino
+++ main.ino
@@ -3,6 +3,16 @@
 Servo servo[4];
 int default_angle[4] = {75, 90, 90, 60};
 
+float z_angle;
+float z_x;
+float z_y;
+float z_length;
+
+float y_angle;
+float y_x;
+float y_y;
+float y_length;
+
 void setup()
 {
     Serial.begin(115200);
@@ -11,10 +21,15 @@ void setup()
     servo[2].attach(5);
     servo[3].attach(2);
 
+    servo_T.attach(4);//grabber twister
+    servo_W.attach(3);//grabber up down
+
     for (size_t i = 0; i < 4; i++)
     {
         servo[i].write(default_angle[i]);
     }
+    servo_T.write(45);
+    servo_W.write(90);
 }
 
 byte angle[4];
@@ -23,6 +38,9 @@ long t = millis();
 
 void loop()
 {
+
+    servo_W.write(45);
+    
     if (Serial.available())
     {
         Serial.readBytes(angle, 4);
@@ -44,5 +62,8 @@ void loop()
             servo[i].write(default_angle[i]);
             pre_angle[i] = default_angle[i];
         }
+
+        servo_T.write(45);
+        servo_W.write(90);
     }
 }
